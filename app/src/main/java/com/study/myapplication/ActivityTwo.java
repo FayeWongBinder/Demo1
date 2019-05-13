d929dba40d645e73a3cab542a9b54ffc8d2cef45
David <549014869@qq.com>
2019-05-13 08:26:18 +0800

    增加类
    更改测试

---
+++ app/src/main/java/com/study/myapplication/ActivityTwo.java
@@ -0,0 +1,11 @@
+package com.study.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityTwo extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
